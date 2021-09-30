e139173bc5dff222d7af7e9a76dd73a4b82d4cc2
FE4-Carol <carolabreu81@gmail.com>
2021-09-29 21:22:35 -0300

    Cadastro de eventos

---
+++ cadastro-de-eventos.js
@@ -0,0 +1,28 @@
+// Autora: Carolina Abreu | Dia: 29/09/2021 | Cadastro de enventos
+
+let Nome, DiaDoEvento, DiaAtual, QtdPart, Idade, Data, DiaADiaAtualtual
+
+// entrada de dados:
+Nome = prompt('Qual é o seu nome? ')
+Idade = prompt('Informe sua idade')
+
+Data = new Date()
+DiaAtual = (Data.getDate() + '-' + (Data.getMonth() + 1) + '-' + Data.getFullYear()).split('-') 
+
+if (Idade > 18){
+    DiaDoEvento = prompt('Qual é o dia do Evento que você quer cadastrar? ').replaceAll('/', '') 
+    if (DiaDoEvento > DiaAtual){
+        QtdPart = prompt('Informe a quantidade de participantes: ')
+        if (QtdPart > 100) {
+            alert('O evento esta lotado! Total de: ' + QtdPart + ' participantes')
+        } else {
+            alert('O evento foi cadastrado com sucesso, quantidade de participantes: ' + QtdPart)
+        }
+    } else {
+        alert('A data do evento é inválida!')
+    }
+} else {
+    alert('Você não idade para cadastrar um evento, sua idade é ' + Idade)
+}
+
+
